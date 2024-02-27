6281a8fb9d329c6a547bf48771b7445e3891a470
Meesam <meesam.engineer@gmail.com>
2024-02-27 08:13:12 +0530

    header Ui complete

--- BlazorAirBnb.Web/Program.cs
+++ BlazorAirBnb.Web/Program.cs
@@ -1,10 +1,12 @@
 using BlazorAirBnb.Web.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
 
