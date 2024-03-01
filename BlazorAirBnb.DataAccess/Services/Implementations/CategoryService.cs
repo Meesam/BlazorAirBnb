dc3cf4302ec166a00dfed8b59b4b8484dd1b98c4
Meesam <meesam.engineer@gmail.com>
2024-03-01 19:14:27 +0530

    add service for category

---
+++ BlazorAirBnb.DataAccess/Services/Implementations/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using BlazorAirBnb.DataAccess.Services.Interfaces;
+using BlazorAirBnb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAirBnb.DataAccess.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<bool> AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Category>> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorAirBnb.DataAccess/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorAirBnb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAirBnb.DataAccess.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IQueryable<Category>> GetAllCategories();
+        Task<bool> AddCategory(Category category);
+
+        Task DeleteCategory(int id);
+
+        Task<Category> GetCategoryById(int id);
+
+        Task<bool> UpdateCategory(Category category);
+    }
+}
